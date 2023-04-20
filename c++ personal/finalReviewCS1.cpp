4116816260f3c6feca6308d657642e26a46e2752
carlostorres18 <torreccarlos@gmail.com>
2023-04-20 14:21:16 -0500

    Started Final Review for CS1 Final

---
+++ c++ personal/finalReviewCS1.cpp
@@ -0,0 +1,17 @@
+// 2. Given int n, already assigned a value, write the necessary loop/loops to draw a backward slash
+// usign n asterisks
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    // Write the code below
+    for(int i = 0; i < n; i++){
+        cout<<"*"<<endl;
+        i++;
+    }
+    
+}
