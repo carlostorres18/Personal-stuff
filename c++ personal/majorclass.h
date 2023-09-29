303c63f3fbaf27682757f34cc6e5944a26764951
carlostorres18 <torreccarlos@gmail.com>
2023-09-29 18:22:11 -0500

    Continued with OOP C++ on majorClass project.

--- c++ personal/majorclass.h
+++ c++ personal/majorclass.h
@@ -3,6 +3,28 @@ using namespace std;
 
 class SameClassMajor{
     private:
+        string major;
+        string classSubject;
+
+    public:
+        void display(){
+            if(classSubject == major){
+                cout<<"Classroom: You are in Classroom 1"<<endl;
+            }
+            else{
+                cout<<"Classroom: You are in Classroom 2"<<endl;
+            }
+            
+        }
+
+    SameClassMajor(string inMajor = "undecided", string inClassSubject = "unknown"){
+        major = inMajor;
+        classSubject = inClassSubject;
+    }
+};
+
+class Student {
+    private:
         int idNumber;
         string name;
         string major;
@@ -14,29 +36,43 @@ class SameClassMajor{
             cout<<"name: "<<name<<endl;
             cout<<"ID number: "<<idNumber<<endl;
             cout<<"Major: "<<major<<endl;
-            if(classSubject == major){
-                cout<<"Classroom: You are in Classroom 1"<<endl;
-            }
-            else{
-                cout<<"Classroom: You are in Classroom 2"<<endl;
-            }
             
         }
 
-    SameClassMajor(int inIDnumber = 0, string inName = "", string inMajor = "undecided", string inClassSubject = "unknown"){
+    Student(int inIDnumber = 0, string inName = "", string inMajor = "undecided", string inClassSubject = "unknown"){
         idNumber = inIDnumber;
         name = inName;
         major = inMajor;
         classSubject = inClassSubject;
     }
-};
-
-class Student {
 
     // sameClassMajor determines if class subject is same as major
+    Student sameClassMajor(){
+        if(classSubject == major){
+                cout<<"Classroom: You are in Classroom 1"<<endl;
+            }
+            else{
+                cout<<"Classroom: You are in Classroom 2"<<endl;
+            }
+    }
 };
 
 class Course {
+    private:
+    int idNumber;
+    string name;
+    string major;
+
+    public:
+
+    void registerForClass(){
+        if(sameClassMajor() == true){
+            cout<<"You are in class 1"<<endl;
+        }
+        else{
+            cout<<"You are in class 2"<<endl;
+        }
+    }
     // register() method
         // for each Student in enrollment list
         // if student.sameClassMajor() == true
@@ -47,6 +83,12 @@ class Course {
     // display sections
         // 1: std1, std2, ...
         // 2: std4, std7, ...
+    void display(){
+        cout<<"Student's Information: "<<endl;
+        cout<<"name: "<<name<<endl;
+        cout<<"ID number: "<<idNumber<<endl;
+        cout<<"Major: "<<major<<endl;
+    }
 };
 
 Course CSCI2380;
