aa5b4ddea4ae7d31ebdb76e14ecbcd1af8260ba5
carlostorres18 <torreccarlos@gmail.com>
2023-11-06 16:46:22 -0600

    Started searches and algorithms CS2

---
+++ CS2 (COLLEGE)/WEEK 11/Searches.h
@@ -0,0 +1,70 @@
+/*
+    Define a function that will tell whether or not a value occurs in an array
+
+    inputs: array of integers
+            length of array
+            target value we are looking for
+        EX: array {5, 1, 2, 9, 10, -4}
+
+*/
+
+
+bool myFunction(int arr[], int keyNum, int length){
+    for(int i = 0; i < length; i++){
+        if(keyNum == arr[i]){
+            return true;
+        }
+        else if (i == length - 1)
+        {
+            return false;
+        }
+    }
+}
+
+bool seqSearchR(int arr[], int target, int length, int startIndex){
+    // Base Case
+    if(arr[startIndex] == length){
+        return true;
+    }
+    else if(startIndex >= length - 1){
+        return false;
+    }
+
+    // Recursive case
+    else{
+        arr++;
+
+        return seqSearchR(arr, target, length, startIndex + 1);
+
+    }
+}
+
+bool binarySearch(int arr[], int length, int start, int end, int target){
+    start = 0;
+    end = length - 1;
+    int mid;
+
+    while(start <= end){
+        // find the middle index here
+        mid = start + (end-start)/2;
+
+        // check to see if the middle value is the target
+        if(arr[mid] == target){
+            // we found it!
+            return true;
+        }
+        // if the middle value is NOT the target
+        else{
+            // is the middle value smaller than the target?
+            if(arr[mid] < target){
+                // move the end index to one before mid
+                end = mid - 1;
+            }
+            else{
+                // mid value is larger than target
+                start = mid + 1;
+            }
+        }
+    }
+
+}
---
+++ CS2 (COLLEGE)/WEEK 11/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+#include "Searches.h"
+
+int main(){
+    int myArray[6] = {5, 1, 2, 9, 10, -4};
+    myFunction(myArray, 9, 6);
+}
