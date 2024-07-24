b4d7e746409d9eb0f39eb31d4a35e8da60192720
carlostorres18 <torreccarlos@gmail.com>
2024-07-24 14:50:03 -0500

    Finished studying Java 72424

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,27 @@
+import java.awt.*;
+
 class Main {
     public static void main(String[] args){
+        String message = "Hello World!!!";
+        System.out.println(message);
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
+        int x = 15;
+        int y = 10;
+        if(x < 20){
+            if(y == 10){
+                System.out.println("X = 15, Y = 10");
+            }
+            System.out.println("This is if statement!");
+        }
+        else if(x == 30){
+            System.out.println("The value of x is " + x);
+        }
+        else{
+            System.out.println("This is else statement!");
+        }
         System.out.println("hello world!");
     }
 }
